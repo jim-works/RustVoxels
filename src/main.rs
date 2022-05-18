f8b655c9b76b6b55e7b9758eec2773c6a9f86991
Jim <jim4m80@gmail.com>
2022-05-18 17:50:22 -0500

    funky triangle

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,13 @@ fn main() {
     #version 140
     in vec2 position;
     out vec2 my_color;
+    out vec4 pos;
 
     uniform mat4 matrix;
     void main() {
         my_color = position;
         gl_Position = matrix*vec4(position, 0.0, 1.0);
+        pos = gl_Position;
     }
     "#;
 
@@ -38,23 +40,39 @@ fn main() {
         color = vec4(my_color, 0.0, 1.0);
     }
     "#;
+
+    let weird_fragment_shader = r#"
+    #version 140
+    uniform float t;
+    in vec4 pos;
+    out vec4 color;
+
+    void main() {
+        float dist = pos.x*pos.x+pos.y*pos.y+0.1;
+        float angle = atan(pos.y/pos.x);
+        float loop_x = 0.5*(sin(t*angle)+1.1);
+        float loop_y = 0.5*(cos(t*angle)+1.1);
+        color = vec4(dist*loop_x, dist*loop_y, 0.0, 1.0);
+    }
+
+    "#;
     let program =
-        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+        glium::Program::from_source(&display, vertex_shader_src, weird_fragment_shader, None)
             .unwrap();
 
     let vertex1 = Vertex {
-        position: [-0.5, -0.5],
+        position: [-1.0, -1.0],
     };
     let vertex2 = Vertex {
-        position: [0.0, 0.5],
+        position: [0.0, 1.0],
     };
     let vertex3 = Vertex {
-        position: [0.5, -0.25],
+        position: [1.0, -1.0],
     };
     let shape = vec![vertex1, vertex2, vertex3];
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-    let mut t: f32 = -0.5;
+    let mut t: f32 = 0.0;
     event_loop.run(move |event, _, control_flow| {
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
@@ -77,20 +95,18 @@ fn main() {
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         // we update `t`
-        t += 0.002;
-        if t > 0.5 {
-            t = -0.5;
-        }
+        t += 0.2;
 
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
         let uniforms = uniform! {
             matrix: [
-                [t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
                 [0.0, 0.0, 0.0, 1.0f32],
-            ]
+            ],
+            t: t
         };
         target
             .draw(
